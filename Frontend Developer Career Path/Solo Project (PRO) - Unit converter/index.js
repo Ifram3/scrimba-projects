5aaa15d960e21fffa35df8b7a0f5c4976759e69c
Ifram3 <111003934+Ifram3@users.noreply.github.com.>
2025-10-04 00:25:14 +0100

    feat: add javascript functionalities

--- Frontend Developer Career Path/Solo Project (PRO) - Unit converter/index.js
+++ Frontend Developer Career Path/Solo Project (PRO) - Unit converter/index.js
@@ -3,3 +3,15 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const unit = document.getElementById("unit");
+const convert = document.querySelector("button");
+
+convert.addEventListener("click", function() {
+    console.log("worki...");
+})
+
+// function feet(x) {
+//     return Number((x * 3.281).toFixed(3));
+// }
+
+// console.log(feet(31));

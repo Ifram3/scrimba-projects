3c19261a1264fc8a1bbbf3241fe6e26d68c6eb6b
Ifram3 <111003934+Ifram3@users.noreply.github.com.>
2025-10-04 19:40:01 +0100

    feat: add click event listener and a function that computes all the conversions

--- Frontend Developer Career Path/Solo Project (PRO) - Unit converter/index.js
+++ Frontend Developer Career Path/Solo Project (PRO) - Unit converter/index.js
@@ -1,17 +1,37 @@
 /*
 1 meter = 3.281 feet
+? meter = 20 feet
+
+
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
 const unit = document.getElementById("unit");
 const convert = document.querySelector("button");
+const length = document.getElementById("Length");
+const liter = document.getElementById("Liters");
+const kilogram = document.getElementById("Kilograms");
+
+const meterToFt = 3.281;
+const literToGl = 0.264;
+const kiloToPound = 2.204;
 
 convert.addEventListener("click", function() {
-    console.log("worki...");
+    let units = unit.value;
+
+    length.textContent = `${units} meters = 
+    ${Number((units * meterToFt).toFixed(3))}  
+    feet | ${units} feet = 
+    ${Number((units / meterToFt).toFixed(3))} meter`
+
+    liter.textContent = `${units} liters = 
+    ${Number((units * literToGl).toFixed(3))}  
+    gallons | ${units} gallons = 
+    ${Number((units / literToGl).toFixed(3))} liters`
+
+    kilogram.textContent = `${units} kilos = 
+    ${Number((units * kiloToPound).toFixed(3))}  
+    pounds | ${units} pounds = 
+    ${Number((units / kiloToPound).toFixed(3))} kilos`
+    //console.log(calc);
 })
-
-// function feet(x) {
-//     return Number((x * 3.281).toFixed(3));
-// }
-
-// console.log(feet(31));
